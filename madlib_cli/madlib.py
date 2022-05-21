5987e2ab65c20ba6ff287aa0a9c38727ebc11700
Marni Hager <mlh6118@gmail.com>
2022-05-21 14:51:58 -0700

    Completed read_template function.

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -8,4 +8,21 @@
 # 4. send the completed response to the user in the command line
 # 5. write the completed text to a new file.
 
-def welcome_msg
+def welcome_msg():
+    print("Welcome to Madlib CLI.  You will be asked to enter various types "
+          "of words.  These will then be used to fill in the blanks of a "
+          "story. \n")
+
+def read_template(file_location):
+    with open(file_location) as file:
+        data = file.read()
+        data = data.strip() # Need to remove newline character.
+        return data
+
+def parse_template():
+    pass
+
+def merge():
+    pass
+
+welcome_msg()
