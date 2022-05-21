aa663c041060e29bd5eaa0056f0bc157744bc255
Marni Hager <mlh6118@gmail.com>
2022-05-21 14:07:21 -0700

    Added assets folder and files.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Lab - Class 03
+
+## Project: Madlib CLI
+### Author: Marni Hager
+
+### Links and Resources
+N/A
+
+### Setup
+N/A
+
+### How to initialize/run the application
+`python madlib.py`
+
+### Tests
+Initial tests were provided in a file called test_madlib.py.
---
+++ madlib_cli/madlib.py
@@ -0,0 +1,11 @@
+# Madlib
+# A command line application which takes advantage of Python's built-in
+# capabilities for reading and writing files.  The application will:
+# 1. read and parse a template Madlib file
+# 2. prompt a user to submit words for each component
+# 3. populate the tempate with the user provided words in the correct
+# position
+# 4. send the completed response to the user in the command line
+# 5. write the completed text to a new file.
+
+def welcome_msg
